635987c331dc18588f24a4518d62e11ac61935e0
Leonardo Nogueira <leonardo.ngra@gmail.com>
2022-11-30 16:13:50 -0300

    Header e Buscador

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,13 +1,27 @@
+import { useState } from 'react'
+
 import styles from './Cardapio.module.scss'
 import { ReactComponent as Logo } from 'assets/logo.svg'
+import Buscador from './Buscador'
 
 export default function Cardapio() {
+
+    const [busca, setBusca] = useState('')
 
     return (
         <main>
             <nav className={styles.menu}>
                 <Logo />
             </nav>
+            <header className={styles.header}> 
+                <div className={styles.header__text}>
+                    A casa do código e da massa
+                </div>
+            </header>
+            <section className={styles.cardapio}>
+                <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+                <Buscador busca={busca} setBusca={setBusca} />
+            </section>
         </main>
     )
 }
