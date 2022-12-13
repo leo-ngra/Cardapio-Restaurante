8d1b9e1810482252058b04131f0624090753aae5
leo-ngra <leonardo.ngra@gmail.com>
2022-12-13 13:15:50 -0300

    Finalizado

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,12 +1,15 @@
 import TagsPrato from 'components/TagsPrato';
+import {useNavigate } from 'react-router-dom';
 
 import { Prato } from 'types/Prato';
 import styles from './Item.module.scss'; 
 
 export default function Item(props: Prato) {
-  const { title, description, photo } = props;
+  const { id, title, description, photo } = props;
+  const navigate = useNavigate();
+
   return (
-    <div className={styles.item}>
+    <div className={styles.item} onClick={() => navigate(`/prato/${id}`)}>
       <div className={styles.item__imagem}>
         <img src={photo} alt={title} />
       </div>
--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -1,8 +1,10 @@
-import { useNavigate, useParams } from 'react-router-dom';
+import { Route, Routes, useNavigate, useParams } from 'react-router-dom';
 
 import styles from './Prato.module.scss';
 import cardapio from 'data/cardapio.json';
 import TagsPrato from 'components/TagsPrato';
+import NotFound from 'pages/NotFound';
+import PaginaPadrao from 'components/PaginaPadrao';
 
 
 
@@ -13,29 +15,34 @@ export default function Prato() {
   const prato = cardapio.find(item => item.id === Number(id));
   
   if(!prato){
-    return '';
+    return <NotFound />;
   }
 
   return (
-    <>
-      <button className={styles.voltar} onClick={() => navigate(-1)}>
-        {'< Voltar'}
-      </button>      
-      <section className={styles.container}>
-        <h1 className={styles.titulo}>
-          {prato.title}
-        </h1>
-        <div className={styles.imagem}>
-          <img src={prato.photo} alt={prato.title} />
-        </div>
-        <div className={styles.conteudo}>
-          <p className={styles.conteudo__descricao}>
-            {prato.description}
-          </p>
-        </div>
+    <Routes>
+      <Route path='*' element={<PaginaPadrao />} />
+      <Route index element={
+        <>
+          <button className={styles.voltar} onClick={() => navigate(-1)}>
+            {'< Voltar'}
+          </button>      
+          <section className={styles.container}>
+            <h1 className={styles.titulo}>
+              {prato.title}
+            </h1>
+            <div className={styles.imagem}>
+              <img src={prato.photo} alt={prato.title} />
+            </div>
+            <div className={styles.conteudo}>
+              <p className={styles.conteudo__descricao}>
+                {prato.description}
+              </p>
+            </div>
 
-        <TagsPrato {...prato} />
-      </section>
-    </>
+            <TagsPrato {...prato} />
+          </section>
+        </>
+      } />
+    </Routes>
   );
 }
