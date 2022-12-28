3b5a96f0b917100e4e3e1b58f00628e503ec1c20
leo-ngra <leonardo.ngra@gmail.com>
2022-12-28 09:26:10 -0300

    Performance

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -1,12 +1,12 @@
+import { lazy } from 'react';
 import { Route, Routes, useNavigate, useParams } from 'react-router-dom';
 
 import styles from './Prato.module.scss';
 import cardapio from 'data/cardapio.json';
-import TagsPrato from 'components/TagsPrato';
-import NotFound from 'pages/NotFound';
-import PaginaPadrao from 'components/PaginaPadrao';
 
-
+const TagsPrato = lazy(() => import('components/TagsPrato'));
+const NotFound = lazy(() => import('pages/NotFound'));
+const PaginaPadrao = lazy(() => import('components/PaginaPadrao'));
 
 export default function Prato() {
   
--- src/routes.tsx
+++ src/routes.tsx
@@ -7,16 +7,16 @@ import Footer from 'components/Footer';
 const Cardapio = lazy(() => import('pages/Cardapio'));
 const PaginaPadrao = lazy(() => import('components/PaginaPadrao'));
 const Inicio = lazy(() => import('pages/Inicio'));
-const NotFound = lazy(() => import('pages/NotFound'));
 const Prato = lazy(() => import('pages/Prato'));
 const Sobre = lazy(() => import('pages/Sobre'));
+const NotFound = lazy(() => import('pages/NotFound'));
 
 export default function AppRouter() {
   return (
     <main className='container'>
       <Router>
+        <Menu />
         <Suspense fallback={<p>Carregando...</p>}>
-          <Menu />
 
           <Routes>
             <Route path='/' element={<PaginaPadrao />}>
@@ -27,8 +27,8 @@ export default function AppRouter() {
             <Route path='prato/:id' element={<Prato />} />
             <Route path='*' element={<NotFound />} />
           </Routes>
-          <Footer />
         </Suspense>
+        <Footer />
       </Router>
     </main>
   );
